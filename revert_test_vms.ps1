66dcb886557fe160b4dde1f005522c35a5740f87
humbledgeeks-nathan <121733667+humbledgeeks-nathan@users.noreply.github.com>
2023-06-24 00:09:59 -0700

    Manual upload.

---
+++ revert_test_vms.ps1
@@ -0,0 +1,14 @@
+﻿# The name of the snapshot you want to revert back to.
+$snapname = "<snapshot name>"
+
+# The FQDN or IP address of your vCenter Server instance.
+Connect-VIServer -Server <vCenter>
+
+# The name of the folder that contains your nested hosts.
+$vms = Get-VM -Location test-scripts | 
+    ForEach-Object {$vms.name} {
+        Stop-VM $_.name -Confirm:$false
+        Set-VM $_.name -Snapshot $snapname -Confirm:$false
+        Start-VM $_.name -Confirm:$false
+        }
+Disconnect-VIServer -Confirm:$false
